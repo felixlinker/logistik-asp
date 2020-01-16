38500faa33479b3d06e8b9a37fb35e7168ba2a25
felixlinker <linkerfelix@gmail.com>
2020-01-16 18:22:43 +0100

    Move days and tu_per_day const into instance

--- gen.ps1
+++ gen.ps1
@@ -1,11 +1,20 @@
 
 param(
+    [int]$Days = 5,
+    [int]$TuPerDay = 8,
     [int]$SampleSize = 500,
     [string[]]$Options = @(),
     [string[]]$Files = @(".\instance-generation\instance-generator.lp"),
     [string]$OutFile = ".\instance.lp"
 )
 
-clingo $SampleSize --outf=2 --warn none @Options @Files `
+# Write constants to the output file
+@(
+    "#const days=$Days.",
+    "#const tu_per_day=$TuPerDay."
+) -join "`n" | Out-File -Encoding ascii $OutFile
+# Write facts to the output file by getting a random model from the instance
+# generator program
+clingo $SampleSize  -c days=$Days -c tu_per_day=8 --outf=2 --warn none @Options @Files `
     | python.exe .\instance-generation\random-instance.py `
-    | Out-File -Encoding ascii $OutFile
+    | Out-File -Append -Encoding ascii $OutFile
