7c1f457bdae06671bfa36c081501793aa79925a9
felixlinker <linkerfelix@gmail.com>
2020-01-23 00:52:20 +0100

    Print subgraph per day

--- visualization/src/Graph.hs
+++ visualization/src/Graph.hs
@@ -15,15 +15,26 @@ globals = ["edge [colorscheme=\"set19\"];"]
 
 toGraph :: FactProgram -> [String]
 toGraph p =
-    let
-        drives = driveSchedules p
-        starts = truckStarts p
-    in
-        ["digraph TruckSchedule {"]
+    let factories  = map head $ Set.toList $ facts p Map.! "factory"
+        warehouses = map head $ Set.toList $ facts p Map.! "warehouse"
+        drives     = driveSchedules p
+        starts     = truckStarts p
+        days       = consts p Map.! "days"
+        dayPlacesGraph :: Int -> [String]
+        dayPlacesGraph d =
+                let fs = map (`Place` d) factories
+                    ws = map (`Place` d) warehouses
+                in  placesGraph fs ws
+    in  ["digraph TruckSchedule {"]
             ++ globals
-            ++ ( Map.foldr (++) []
-               $ Map.mapWithKey (\k -> toEdgeRec (Place (starts Map.! k) 1)) drives
-               )
+            ++ concatMap dayPlacesGraph [1 .. days + 1]
+            ++ Map.foldr
+                   (++)
+                   []
+                   (Map.mapWithKey
+                       (\k -> toEdgeRec (Place (starts Map.! k) 1))
+                       drives
+                   )
             ++ ["}"]
   where
     toEdgeRec :: Place -> [Drives] -> [String]
@@ -34,6 +45,26 @@ data Place = Place { pid :: Int
                    , pday :: Int } deriving (Eq, Ord)
 instance Show Place where
     show (Place i d) = printf "n%d_%d" i d
+
+type Factory = Place
+type Warehouse = Place
+
+placesGraph :: [Factory] -> [Warehouse] -> [String]
+placesGraph fs ws =
+    let day = pday $ head fs
+    in  [ printf "subgraph cluster_%d {" day
+        , printf "label=\"Day %d\";"     day
+        , "color=black;"
+        ]
+            ++ map (shapedNode factoryShape)   fs
+            ++ map (shapedNode warehouseShape) ws
+            ++ ["}"]
+  where
+    factoryShape   = "triangle"
+    warehouseShape = "house"
+    shapedNode :: String -> Place -> String
+    shapedNode shape p =
+        printf "%s [shape=\"%s\",label=\"%d\"];" (show p) shape (pid p)
 
 data Step = Step { day :: Int
                  , num :: Int } deriving (Eq, Ord)
