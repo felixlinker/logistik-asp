4c95e877834046c64c0d86e3b4888b0e6fc91337
felixlinker <linkerfelix@gmail.com>
2020-01-24 17:05:43 +0100

    Simplify final example

--- gen-final.ps1
+++ gen-final.ps1
@@ -2,6 +2,10 @@
             -Options "-c max_consumes=10", `
                      "-c max_produces=10", `
                      "-c min_cost=2", `
+                     "-c factory_num=3", `
+                     "-c warehouse_num=3", `
+                     "-c resource_num=3", `
+                     "-c product_num=3", `
                      "--rand-freq=0.05" `
             -Files  .\instance-generation\instance-generator.lp, `
                     .\instance-generation\final-instance-constraints.lp `
