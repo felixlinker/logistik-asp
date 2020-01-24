9c1c160d4e5efe2f5abc8317f6d99c368f2ca82e
felixlinker <linkerfelix@gmail.com>
2020-01-24 15:30:04 +0100

    Spice up final example

--- gen-final.ps1
+++ gen-final.ps1
@@ -1,6 +1,8 @@
 .\gen.ps1   -SampleSize 100 `
-            -Options "-c max_consumes=20", `
-                     "-c max_produces=20" `
+            -Options "-c max_consumes=10", `
+                     "-c max_produces=10", `
+                     "-c min_cost=2", `
+                     "--rand-freq=0.05" `
             -Files  .\instance-generation\instance-generator.lp, `
                     .\instance-generation\final-instance-constraints.lp `
             -OutFile .\final-instance.lp
