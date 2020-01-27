1753be8027ded01a9ed87bad986e40401395fd5b
Felix Linker <linkerfelix@gmail.com>
2020-01-27 21:38:51 +0100

    Further simplify final example

--- gen-final.ps1
+++ gen-final.ps1
@@ -1,11 +1,11 @@
 .\gen.ps1   -SampleSize 100 `
-            -Options "-c max_consumes=10", `
-                     "-c max_produces=10", `
+            -Options "-c max_consumes=8", `
+                     "-c max_produces=8", `
                      "-c min_cost=2", `
-                     "-c factory_num=3", `
-                     "-c warehouse_num=3", `
-                     "-c resource_num=3", `
-                     "-c product_num=3", `
+                     "-c factory_num=2", `
+                     "-c warehouse_num=2", `
+                     "-c resource_num=2", `
+                     "-c product_num=2", `
                      "--rand-freq=0.05" `
             -Files  .\instance-generation\instance-generator.lp, `
                     .\instance-generation\final-instance-constraints.lp `
